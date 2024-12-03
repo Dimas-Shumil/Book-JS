fa2ae7bf97e6d550ee6ede08293ad8249fca30bc
Dmitry Shumilov <shum0@mail.ru>
2024-12-03 11:37:00 +0700

    javaScript теория

--- site/script/js.js
+++ site/script/js.js
@@ -1,8 +1,20 @@
-// кнопка меню ссылок
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+// // кнопка меню ссылок
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//   }
+
+// Ширина экрана
+
+// const button = document.querySelector('button');
+
+// const updateWidthLabel = () => {
+//     const currentWidth = window.innerWidth;
+//     button.innerHTML = `Ширина экрана ${currentWidth} пикселей`
+// }
+
+// updateWidthLabel();
+// window.addEventListener('resize', updateWidthLabel);
